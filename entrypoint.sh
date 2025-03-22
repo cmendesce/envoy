8cc3278afe3d38dfb394d510822abc3388543ebc
Carlos Mendes <cmendesce@gmail.com>
2025-03-22 12:56:37 +0000

    fixing filter name

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-sed -i "s/FAULT_PERCENTAGE_PLACEHOLDER/${FAULT_PERCENTAGE:-100}/" /etc/envoy/envoy.yaml
+sed -i "s/FAULT_PERCENTAGE_PLACEHOLDER/${FAULT_PERCENTAGE:-0}/" /etc/envoy/envoy.yaml
 sed -i "s/PORT_PLACEHOLDER/${BACKEND_PORT:-80}/" /etc/envoy/envoy.yaml
 exec envoy -c /etc/envoy/envoy.yaml
